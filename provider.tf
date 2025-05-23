51040ecc64746354a26cb52507eb0441903e9288
PaulineGB <pauline.gb@protonmail.com>
2025-05-23 14:48:59 +0200

    initialise terraform project

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = "1.12.1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.98.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-3"
+}
